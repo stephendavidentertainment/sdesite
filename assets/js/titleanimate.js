86a1aba938fc6c5d976514b82e291210242749ec
Randy <boo13bot@gmail.com>
2024-10-31 12:23:00 -0400

    jpg to png

--- assets/js/titleanimate.js
+++ assets/js/titleanimate.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         {
             title: "The Titans That Built America",
-            heroSrc: "https://stephendavidentertainment.github.io/sdesite/images/landing/TitansNew2.jpg",
+            heroSrc: "https://stephendavidentertainment.github.io/sdesite/images/landing/TitansNew2.png",
             heroAlt: "The Titans That Built America",
             laurelsSrc: "https://stephendavidentertainment.github.io/sdesite/images/laurels/Titans_Laurels.png"
         },
